9a027e97264bc07c5054e6fa8e4ccd842c1b33ee
emreucbudak <147994857+emreucbudak@users.noreply.github.com>
2023-10-16 21:12:55 +0300

    Add files via upload

---
+++ Hesap Makinesi.c
@@ -0,0 +1,64 @@
+/******************************************************************************
+
+                            Emre Üçbudak
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int x , y , z , t = 0 , b = 0;
+    double a;
+    jump:
+    printf("ISLEM NUMARALARI = \n1-Toplama\n2-Çıkarma\n3-Bölme\n4-Çarpma\n");
+    printf("ISLEM SECIN = ");
+    scanf("%d",&x);
+    if (x<1)
+    {
+        printf ("İşlem Bulunamadı!");
+        goto jump;
+    }
+    if (x>4)
+    {
+        printf ("İşlem Bulunamadı \n");
+        goto jump;
+        
+    }
+    printf("1. Sayiyi Giriniz = ");
+    scanf("%d",&y);
+    printf("2. Sayiyi Giriniz = ");
+    scanf("%d", &z);
+    if (x==1)
+    {
+        int e;
+        t = y+z;
+        printf("İşlem Sonucu = %d ", t);
+    }    
+    if (x==2)
+    {
+        t = y-z;
+        printf("İşlem Sonucu = %d", t);
+        
+    }
+    if (x==3)
+    {
+        t = y%z;
+        printf("Kalan = %d\n",t);
+        if(y<z)
+        {
+            printf ("Küçük Sayı Büyük Sayıya Bölünemez");
+        }
+        else
+        {
+            a = y/z;
+            printf ("Bölüm = %f", a);
+            
+        }
+    }
+    if (x==4)
+    {
+        t = y*z;
+        printf("İşlem Sonucu = %d",t);
+    }
+    return 0;
+}
